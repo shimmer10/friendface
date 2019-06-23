2869d10d08c115d06314cb50d694242c720131a5
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-06-23 16:20:55 -0400

    some updates for survey to be pushed to the data file/still can't push

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@
  * Friendface Api Routes
  ************************************/
 
- // file to store and retrieve friend data
+// file to store and retrieve friend data
 var friendData = require("../data/friends");
 
 // routing
@@ -15,21 +15,14 @@ module.exports = function (app) {
   // Displays all possible friends
   app.get("/api/friends", function (req, res) {
     console.log(friends);
-    // return res.json(friends);
+    return res.json(friends);
   });
 
   // Create New Friend
   app.post("/api/friends", function (req, res) {
-    var newFriend = req.body;
+    friendData.push(req.body);
 
-    // remove spaces from newFriend
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newFriend);
-
-      friendsData.push(newFriend);
-
-      // res.json(newFriend);
+    res.json(req.body);
   });
 };
 
